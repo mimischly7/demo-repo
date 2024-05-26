c824b6e65089827eb017ed1817c4e3d039153033
Mimis Chlympatsos <mimis.chlympatsos@mail.utoronto.ca>
2024-05-26 13:52:58 -0400

    ppp

--- setup.py
+++ setup.py
@@ -8,4 +8,14 @@ setup(
         # List your project's dependencies here, e.g.,
         # 'some_package>=1.0.4',
     ],
+    author='Your Name',  # Add your name
+    author_email='your.email@example.com',  # Add your email
+    description='A description of your package',  # Add a short description
+    url='https://github.com/username/repository',  # Add the URL of your repository
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Specify the minimum Python version required
 )
