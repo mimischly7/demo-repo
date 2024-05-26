1f9521714bca941195cb1018d072c695894b0afa
Mimis Chlympatsos <mimis.chlympatsos@mail.utoronto.ca>
2024-05-26 12:58:05 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='test',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your project's dependencies here, e.g.,
+        # 'some_package>=1.0.4',
+    ],
+)
